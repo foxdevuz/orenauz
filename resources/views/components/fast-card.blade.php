0dd63e51f5e09e257f5954f98aae566da02f2e21
AbdullajonovBobur <abdullajonovboburjon05@gmail.com>
2023-03-08 22:50:57 +0500

    Porject started | 1st section

--- resources/views/components/fast-card.blade.php
+++ resources/views/components/fast-card.blade.php
@@ -5,7 +5,7 @@
     </div>
     <div class="card-body">
         <x-news-title style="font-size: 1.3rem;">Ilon Mask Trampni twitter sahifaini blokdan chiqardi</x-news-title>
-        <x-news-little-text class="pt-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente necessitatibus eligendi quasi harum quia ad accusamus excepturi fugiat, atque minima?</x-news-little-text>
+        <x-news-little-text class="pt-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente necessitatibus eligendi</x-news-little-text>
         <p class="more"><a href="#">Ko'proq o'qish...</a></p>
     </div>
 </div>
--- resources/views/components/popular-news-card.blade.php
+++ resources/views/components/popular-news-card.blade.php
@@ -2,7 +2,8 @@
     <div class="body">
         <x-category>Biznes</x-category>
         <p> <i class="fa-regular fa-calendar"></i> 22/02/2022</p>
-        <p class="news-title fs-6"><a href="#">Ilon Mask Trampni twitter sahifaini blokdan chiqardi</a></p>
+        <p class="news-title extra"><a href="#">Ilon Mask Trampni twitter sahifaini blokdan chiqardi</a></p>
+        <p class="more"><a href="#">Ko'proq o'qish...</a></p>
     </div>
     <div class="image">
         <img src="/images/testImage.jpg" alt="News Image">
