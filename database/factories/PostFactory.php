2491f93188ff11c67790dec6eb6612309803a437
Foxdevuz <abdullajonovboburjon05@gmail.com>
2023-05-07 22:02:55 +0500

    Post Factrory, Model, Migration are added

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name'=>fake()->paragraph(),
+            'slug'=>fake()->slug(),
+            'description'=>fake()->text(),
+            'view'=>fake()->numerify(),
+        ];
+    }
+}
